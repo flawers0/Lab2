75b64a30e4a839fdf0d288f6a3e068b4a8429170
flawers0 <as.pronin17@gmail.com>
2025-09-24 09:29:30 +0300

    Update of formatting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+# Бригада №8 Пронін Артем, Новіков Іван, Дмитрій Корольов, Анастасія Пальчик
 a = ["Artem Pronin", "Novikov Ivan", "Dmytrii Korolev", "Anastasia Palchick"]
 
 for name in a:
     for char in name:
         code = ord(char)
-        print(f"{char}\n10: {code}\n2: {bin(code)[2:]}\n16: {hex(code)[2:]}\n")
+        print(f"{char} - {code} - {bin(code)[2:]} - {hex(code)[2:]}")
